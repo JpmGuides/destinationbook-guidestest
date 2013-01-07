2c93e0ef059de1b3b596af22753619c9c05da2db
Néville Dubuis <neville.dubuis@liquid-concept.ch>
2013-01-07 15:20:14 +0100

    redo start_up script

--- script/startup.sh
+++ script/startup.sh
@@ -1,8 +1,6 @@
 #! /bin/bash
 
 cd /var/www/local_guide
-ps -w | grep r[a]ke | cut -c-9 | xargs env kill
-sleep 5
 git pull origin server
 touch tmp/restart.txt
-rake listner:start
+ruby lib/listner.rb restart
